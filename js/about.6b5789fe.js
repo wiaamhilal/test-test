04c5a7db7fe9a0ba9d45bba85b0e560b399933f0
wiaamhilal <weaam224112@gmail.com>
2023-06-16 04:33:14 -0700

    deploy

---
+++ js/about.6b5789fe.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwiaam_proj"]=self["webpackChunkwiaam_proj"]||[]).push([[443],{5399:function(n,t,u){u.r(t),u.d(t,{default:function(){return l}});var a=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],r=u(1001),e={},i=(0,r.Z)(e,a,s,!1,null,null,null),l=i.exports}}]);
+//# sourceMappingURL=about.6b5789fe.js.map
